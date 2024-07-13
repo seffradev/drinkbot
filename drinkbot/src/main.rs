2e66622f1952747e6c806d617bc02608f45313eb
Hampus Avekvist <hampus.avekvist@hey.com>
2024-07-13 23:49:41 +0200

    feat: use poise and slash commands instead of message parsing

--- drinkbot/src/main.rs
+++ drinkbot/src/main.rs
@@ -1,68 +1,59 @@
 use std::env;
 
 use lazy_static::lazy_static;
-use serenity::{async_trait, model::channel::Message, prelude::*};
+use poise::serenity_prelude as serenity;
 use thecocktaildb_rs::{Client as CocktailClient, Cocktails};
-use tracing::{error, warn};
+use tracing::warn;
 
-struct Handler;
+struct Data;
+type Context<'a> = poise::Context<'a, Data, anyhow::Error>;
 
 lazy_static! {
     static ref COCKTAIL: CocktailClient =
         CocktailClient::new(&env::var("THECOCKTAILDB_TOKEN").expect("Expected THECOCKTAILDB_TOKEN in the environment")).unwrap();
 }
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!drink" {
-            let cocktail = match Cocktails::random(&COCKTAIL).await {
-                Ok(cocktails) => cocktails,
-                Err(err) => {
-                    error!("{}", err);
-                    return;
-                }
-            };
+/// Fetches a random drink
+#[poise::command(slash_command, prefix_command)]
+async fn random(ctx: Context<'_>) -> Result<(), anyhow::Error> {
+    let cocktail = Cocktails::random(&COCKTAIL).await?;
 
-            let message = cocktail.first().map_or("no drink found".into(), |cocktail| {
-                if cocktail.drink.is_none() {
-                    return "no drink found".into();
-                }
-
-                if cocktail.ingredients.is_empty() {
-                    return "no drink found".into();
-                }
-
-                let ingredients = cocktail
-                    .ingredients
-                    .iter()
-                    .filter(|i| i.ingredient.is_some())
-                    .filter(|i| i.measure.is_some())
-                    .map(|i| format!("{} ({})", i.ingredient.clone().unwrap().trim(), i.measure.clone().unwrap().trim()))
-                    .collect::<Vec<_>>();
-
-                if cocktail.instructions.en.is_none() {
-                    return "no drink found".into();
-                }
-
-                if cocktail.glass.is_none() {
-                    return "no drink found".into();
-                }
-
-                format!(
-                    "**Name**: {}\n**Ingredients**: {}\n**Instructions**: {}\n**Glass**: {}",
-                    cocktail.drink.clone().unwrap().trim(),
-                    ingredients.join(", ").trim(),
-                    cocktail.instructions.en.clone().unwrap().trim(),
-                    cocktail.glass.clone().unwrap().trim(),
-                )
-            });
-
-            if let Err(why) = msg.channel_id.say(&ctx.http, message).await {
-                println!("Error sending message: {why:?}");
-            }
+    let message = cocktail.first().map_or("no drink found".into(), |cocktail| {
+        if cocktail.drink.is_none() {
+            return "no drink found".into();
         }
-    }
+
+        if cocktail.ingredients.is_empty() {
+            return "no drink found".into();
+        }
+
+        let ingredients = cocktail
+            .ingredients
+            .iter()
+            .filter(|i| i.ingredient.is_some())
+            .filter(|i| i.measure.is_some())
+            .map(|i| format!("{} ({})", i.ingredient.clone().unwrap().trim(), i.measure.clone().unwrap().trim()))
+            .collect::<Vec<_>>();
+
+        if cocktail.instructions.en.is_none() {
+            return "no drink found".into();
+        }
+
+        if cocktail.glass.is_none() {
+            return "no drink found".into();
+        }
+
+        format!(
+            "**Name**: {}\n**Ingredients**: {}\n**Instructions**: {}\n**Glass**: {}",
+            cocktail.drink.clone().unwrap().trim(),
+            ingredients.join(", ").trim(),
+            cocktail.instructions.en.clone().unwrap().trim(),
+            cocktail.glass.clone().unwrap().trim(),
+        )
+    });
+
+    ctx.say(message).await?;
+    Ok(())
 }
 
 #[tokio::main]
@@ -74,11 +65,24 @@ async fn main() -> Result<(), anyhow::Error> {
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![random()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data)
+            })
+        })
+        .build();
 
     // Create a new instance of the Client, logging in as a bot.
-    let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
+    let mut client = serenity::Client::builder(&token, intents)
+        .framework(framework)
         .await
         .expect("Err creating client");
 
